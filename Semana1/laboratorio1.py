f20a0af4fb8f0f76e044a9af3538c5f491ee0ffb
laptophp <santiagodelacrvz@gmail.com>
2025-01-26 08:45:33 -0600

    create tarea

---
+++ Semana1/laboratorio1.py
@@ -0,0 +1,55 @@
+'''
+1.14 Ejercicios
+Ejercicio 1: ¿Cuál es la función de la memoria secundaria en una computadora?
+    a) Ejecutar todos los cálculos y la lógica del programa
+    b) Descargar páginas web de Internet
+    >>>c) Almacenar información durante mucho tiempo, incluso después de ciclos de
+    apagado y encendido
+    d) Recolectar la entrada del usuario
+Ejercicio 2: ¿Qué es un programa?
+    Es un conjunto de instrucciones o comandos que se le da a una computadora a interpretar para que esta realice multiples tareas 
+Ejercicio 3: ¿Cuál es la diferencia entre un compilador y un intérprete?
+        El inetprete: toma nuestro codigo y lo traduce linea por linea a lenguaje maquina que lo pueda decifrar, es mas lento , y no requiere de crear un documento ejecutable aparte 
+        El compilador: Traduce nuetro codiogo el codigo fuente a enguaje maquina en un solo paso, crea un archivo ejecotsble y todo esto lo hace antes de ejecutar el programa
+Ejercicio 4: ¿Cuál de los siguientes contiene “código máquina”?
+    a) El intérprete de Python
+    b) El teclado
+    >>>c) El código fuente de Python
+    d) Un documento de un procesador de texto
+Ejercicio 5: ¿Qué está mal en el siguiente código?:
+ >>> primt ¡Hola, mundo!
+ File "<stdin>", line 1
+ primt ¡Hola, mundo!
+ ˆ
+ SyntaxError: invalid syntax
+ >>>
+la palabra print tiene la m en lugar de la n ademas le faltan los ()
+
+ Ejercicio 6: ¿En qué lugar del computador queda almacenada una variable, como
+ en este caso “X”, después de ejecutar la siguiente línea de Python?:
+ x = 123
+    a) Unidad central de procesamiento
+    >>>b) Memoria Principal
+    c) Memoria Secundaria
+    d) Dispositivos de Entrada
+    e) Dispositivos de Salida
+ Ejercicio 7: ¿Qué mostrará en pantalla el siguiente programa?:
+ x = 43
+ x = x + 1
+ print(x)
+ a) 43
+ >>>b) 44
+ c) x + 1
+ d) Error, porque x = x + 1 no es posible matemáticamente.
+Ejercicio 8: Explica cada uno de los siguientes conceptos usando un ejemplo de
+una capacidad humana: 
+(1) Unidad central de procesamiento, : El cerebro 
+(2) Memoria principal,:Memoria a corto plazo  
+(3) Memoria secundaria,:memoria a largo plazo 
+(4) Dispositivos de entrada, :los sentidos  
+(5) Dispositivos desalida. .las acciones que realizamos  
+Por ejemplo, “¿Cuál sería el equivalente humano de la Unidad central de
+ procesamiento?”.
+ Ejercicio 9: ¿Cómo puedes corregir un “Error de sintaxis”?.
+ Revisar las palabras reservadas y las vaiables y los operadose en busca de algo que este mal escrito 
+'''
