e6de117639064c1425bc88bedc7e2b6ff9f6605a
laptophp <santiagodelacrvz@gmail.com>
2025-02-15 22:05:12 -0600

    nuevo

--- 100ejercicios/ejercicio10.py
+++ 100ejercicios/ejercicio10.py
@@ -1,21 +1,33 @@
-
-with open('C:\\Users\\santi\\OneDrive\\Documentos\\GitHub\\PROGBAS-FCFM-LEONARDO\\100ejercicios\\miarchivito.txt', 'r', encoding='utf-8') as archivo:
-    contenido = archivo.read()
+path = "C:\\Users\\santi\\OneDrive\\Documentos\\GitHub\\PROGBAS-FCFM-LEONARDO\\100ejercicios\\"
+name = "miarchivito"
+ext = "txt"
+# Profe no se porque al poner la / no puede dar acceso ero si son la \\
+with open(path+name+"."+ext, 'r', encoding='utf-8') as miarchivito:
+    contenido = miarchivito.read()
     print(contenido)
 
 
-with open('C:\\Users\\santi\\OneDrive\\Documentos\\GitHub\\PROGBAS-FCFM-LEONARDO\\100ejercicios\\miarchivito.txt', 'w', encoding='utf-8') as archivo:
-    archivo.write('Este es el nuevo contenido del archivo.\n')
+with open(path+name+"."+ext, 'w', encoding='utf-8') as miarchivito:
+    miarchivito.write('Este es el nuevo contenido del archivo.\n')
+    
+with open(path+name+"."+ext, 'r', encoding='utf-8') as miarchivito:
+    contenido = miarchivito.read()
+    print(contenido)
 
-with open('C:\\Users\\santi\\OneDrive\\Documentos\\GitHub\\PROGBAS-FCFM-LEONARDO\\100ejercicios\\miarchivito.txt', 'a', encoding='utf-8') as archivo:
-    archivo.write('Esta es una línea añadida al archivo.\n')
-with open('miarchivito.txt', 'r', encoding='utf-8') as archivo:
-    lineas = archivo.readlines()
+with open(path+name+"."+ext, 'a', encoding='utf-8') as miarchivito:
+    miarchivito.write('Esta es una línea añadida al archivo.\n')
+with open(path+name+"."+ext, 'r', encoding='utf-8') as miarchivito:
+    contenido = miarchivito.read()
+    print(contenido)
+
+with open(path+name+"."+ext, 'r', encoding='utf-8') as miarchivito:
+    lineas = miarchivito.readlines()
 
 
 lineas[0] = 'Esta es la línea modificada.\n'
 
 
-with open('C:\\Users\\santi\\OneDrive\\Documentos\\GitHub\\PROGBAS-FCFM-LEONARDO\\100ejercicios\\miarchivito.txt', 'w', encoding='utf-8') as archivo:
-    archivo.writelines(lineas)
+with open(path+name+"."+ext,  'w', encoding='utf-8') as miarchivito:
+    miarchivito.writelines(lineas)
 
+miarchivito.close()
